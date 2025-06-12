44d045a3720a72d22e7e1540db1b8bd9d4035cd9
RAHUL SAGAR PABBA <100937627+rahulsagar500@users.noreply.github.com>
2025-06-13 05:58:52 +1000

    Initial Commit containing chatbot stubs

---
+++ backend/src/app/chatbot.py
@@ -0,0 +1,14 @@
+"""Chatbot Deployment logic"""
+from transformers import pipelines
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+class Chatbot:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def respond(self):
+        pass
